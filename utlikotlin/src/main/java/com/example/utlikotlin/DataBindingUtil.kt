5fbc6377c0533ae9f68458a58df682e4803c0600
Ziliang Chen <c.ym62942@gmail.com>
2020-11-08 23:43:24 -0500

    Add - DataBinding & Extension Function
    Added BindingAdapter functions and extension functions for rounding decimals.

---
+++ utlikotlin/src/main/java/com/example/utlikotlin/DataBindingUtil.kt
@@ -0,0 +1,13 @@
+package com.example.utlikotlin
+
+object DataBindingUtil {
+    @BindingAdapter("android:text", "isAddCase")
+    @JvmStatic
+    fun setText(editText: EditText, text: String, isAddCase: Boolean) {
+        if (isAddCase) editText.setText("") else editText.setText(text)
+    }
+
+    @InverseBindingAdapter(attribute = "android:text")
+    @JvmStatic
+    fun getText(editText: EditText) = editText.text.toString().trim()
+}
