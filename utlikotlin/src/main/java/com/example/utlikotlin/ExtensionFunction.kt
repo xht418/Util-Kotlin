36ac84eccbeb8e7ba8c6035ff87292189dc58dda
Ziliang Chen <c.ym62942@gmail.com>
2020-12-26 11:12:16 -0500

    Add - ExtensionFunction (2)
    Added 2 extension functions, one for conveting dp to pixel, on for simple scaling animation.

--- utlikotlin/src/main/java/com/example/utlikotlin/ExtensionFunction.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/ExtensionFunction.kt
@@ -1,5 +1,23 @@
 package com.example.utlikotlin
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.content.Context
+import android.view.View
+
 fun Double.roundDecimal(digit: Int) = "%,.${digit}f".format(this)
 
 fun <T> List<T>.range(fromIndex: Int, toIndex: Int) = this.subList(fromIndex, toIndex + 1)
+
+fun Int.dp(context: Context) = this * context.resources.displayMetrics.density
+
+fun View.scale(value: Float, duration: Long) {
+    val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, value)
+    val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, value)
+
+    val animator = ObjectAnimator.ofPropertyValuesHolder(this, scaleX, scaleY)
+
+    animator.duration = duration
+
+    animator.start()
+}
