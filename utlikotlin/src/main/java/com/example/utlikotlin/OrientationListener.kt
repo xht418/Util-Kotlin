300b68efbf699c8bf6507186c277ac16f13d7ba6
Ziliang Chen <c.ym62942@gmail.com>
2021-04-15 16:23:03 -0400

    Add - New Class (1)
    Added 1 new util class called "OrientationListener" for reacting to orientation change when using ExoPlayer.

---
+++ utlikotlin/src/main/java/com/example/utlikotlin/OrientationListener.kt
@@ -0,0 +1,78 @@
+package com.example.utlikotlin
+
+import android.content.Context
+import android.view.OrientationEventListener
+
+class OrientationListener(
+    private val context: Context,
+    private val degree0Action: () -> Unit,
+    private val degree90Action: () -> Unit,
+    private val degree180Action: () -> Unit,
+    private val degree270Action: () -> Unit,
+    var isFullScreenModeToggled: Boolean = false
+) : OrientationEventListener(context) {
+    companion object {
+        const val TOLERANCE = 20
+    }
+
+    private var isDisablePortrait = false
+    private var isDisableLandscape = false
+
+    private fun reset() {
+        isFullScreenModeToggled = false
+        isDisablePortrait = false
+        isDisableLandscape = false
+    }
+
+    override fun onOrientationChanged(degree: Int) {
+        if (SensorHelper.isAutoRotateOn(context)) {
+            if (isFullScreenModeToggled) {
+                if (Device.isLandscapeMode(context)) {
+                    isDisablePortrait = true
+                    isDisableLandscape = false
+                } else {
+                    isDisableLandscape = true
+                    isDisablePortrait = false
+                }
+            }
+
+            when (degree) {
+                in (0..0 + TOLERANCE) -> {
+                    if (!isDisablePortrait) {
+                        degree0Action()
+
+                        reset()
+                    }
+                }
+
+                in (359 - TOLERANCE..359) -> {
+                    if (!isDisablePortrait) {
+                        degree0Action()
+
+                        reset()
+                    }
+                }
+
+                in (90 - TOLERANCE..90 + TOLERANCE) -> {
+                    degree90Action()
+
+                    reset()
+                }
+
+                in (180 - TOLERANCE..180 + TOLERANCE) -> {
+                    degree180Action()
+
+                    reset()
+                }
+
+                in (270 - TOLERANCE..270 + TOLERANCE) -> {
+                    if (!isDisableLandscape) {
+                        degree270Action()
+
+                        reset()
+                    }
+                }
+            }
+        }
+    }
+}
