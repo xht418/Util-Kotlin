1bf1ffb3260c32e3a39f1b226feec03e72f1bdc4
Ziliang Chen <c.ym62942@gmail.com>
2021-04-19 11:11:49 -0400

    Add - ExtensionFunction (1)
    Added 1 extension function for setting full screen mode on/off.

--- utlikotlin/src/main/java/com/example/utlikotlin/ExtensionFunction.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/ExtensionFunction.kt
@@ -291,3 +291,13 @@ fun AndroidViewModel.getRawUris(context: Context, arrayResourceId: Int): List<Ur
 
     return rawUris
 }
+
+fun Fragment.setFullScreenMode(isEnable: Boolean) {
+    if (isEnable) {
+        val systemUiFlag = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+        requireActivity().window.decorView.systemUiVisibility = systemUiFlag
+    } else {
+        requireActivity().window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    }
+}
