b4a72ff5135887ceca7748959f4a6d3ec3181015
Ziliang Chen <c.ym62942@gmail.com>
2021-05-25 14:52:44 -0400

    Add - Keyboard (2)
    Added 2 functions for Keyboard class: showing/hiding the keyboard in dialog.

--- utlikotlin/src/main/java/com/example/utlikotlin/Keyboard.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/Keyboard.kt
@@ -1,15 +1,27 @@
 package com.example.utlikotlin
 
 import android.app.Activity
-import android.app.Service
+import android.content.Context
 import android.view.inputmethod.InputMethodManager
 
 object Keyboard {
     fun hide(activity: Activity) {
-        val inputMethodManager = activity.getSystemService(Service.INPUT_METHOD_SERVICE) as InputMethodManager
+        val inputMethodManager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
         activity.currentFocus?.let {
             inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
         }
+    }
+
+    fun showInDialog(context: Context) {
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+    }
+
+    fun hideInDialog(context: Context) {
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
     }
 }
