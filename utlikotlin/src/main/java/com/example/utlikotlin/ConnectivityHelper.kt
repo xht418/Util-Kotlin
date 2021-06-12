a6c9f9399f0ad233e33b6eb0ae8a4f63d4658fb8
Ziliang Chen <c.ym62942@gmail.com>
2021-06-12 09:31:25 -0400

    Add - ConnectivityHelper (1)
    Added 1 helper function for getting IP address in current WIFI environment.

--- utlikotlin/src/main/java/com/example/utlikotlin/ConnectivityHelper.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/ConnectivityHelper.kt
@@ -2,9 +2,21 @@ package com.example.utlikotlin
 
 import android.content.Context
 import android.net.NetworkCapabilities
+import android.net.wifi.WifiManager
+import java.net.InetAddress
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 object ConnectivityHelper {
     fun getIsWifiOn(context: Context) = with(context.getConnectivityManager()) {
         getNetworkCapabilities(activeNetwork)?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+    }
+
+    fun getIpAddress(context: Context): String {
+        val wifiManager = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        return wifiManager.connectionInfo.ipAddress.let {
+            InetAddress.getByAddress(ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(it).array()).hostAddress
+        }
     }
 }
