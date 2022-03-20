4f2dc371f4bbfa513d257dd9700cbe13fab5f709
Ziliang Chen <c.ym62942@gmail.com>
2022-03-20 10:20:42 -0400

    Update: ConnectivityHelper
    Updated 1 function "getIpAddress()", replaced the deprecated methods with the latest.

--- utlikotlin/src/main/java/com/example/utlikotlin/ConnectivityHelper.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/ConnectivityHelper.kt
@@ -2,10 +2,6 @@ package com.example.utlikotlin
 
 import android.content.Context
 import android.net.NetworkCapabilities
-import android.net.wifi.WifiManager
-import java.net.InetAddress
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
 
 object ConnectivityHelper {
     fun isWifiOn(context: Context) = with(context.getConnectivityManager()) {
@@ -18,11 +14,7 @@ object ConnectivityHelper {
 
     fun isNetworkAvailable(context: Context) = isWifiOn(context) || isDataOn(context)
 
-    fun getIpAddress(context: Context): String {
-        val wifiManager = context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-
-        return wifiManager.connectionInfo.ipAddress.let {
-            InetAddress.getByAddress(ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(it).array()).hostAddress
-        }
+    fun getIpAddress(context: Context) = with(context.getConnectivityManager()) {
+        getLinkProperties(activeNetwork)!!.linkAddresses[1].address.hostAddress!!
     }
 }
