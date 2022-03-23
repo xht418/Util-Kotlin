e6410b1a44d03b775cd4353288bac1361b8cacbb
Ziliang Chen <c.ym62942@gmail.com>
2022-03-23 11:36:33 -0400

    Add: New Class (1)
    Added 1 new class called "DataStore" , which uses PreferenceDataStore api to store user preferences data. The original "DataStore" class was renamed to "DataStore2", which may be deleted in the future.

---
+++ utlikotlin/src/main/java/com/example/utlikotlin/DataStore2.kt
@@ -0,0 +1,54 @@
+package com.example.utlikotlin
+
+import android.content.Context
+import android.net.Uri
+import androidx.core.content.edit
+import androidx.core.net.toUri
+
+object DataStore2 {
+    fun saveImageUri(context: Context, imageName: String, imageUri: Uri) {
+        val sharedPreferences = context.getSharedPreferences("images", Context.MODE_PRIVATE)
+
+        sharedPreferences.edit {
+            putString(imageName, imageUri.toString())
+        }
+    }
+
+    fun getImageUri(context: Context, imageName: String): Uri? {
+        val sharedPreferences = context.getSharedPreferences("images", Context.MODE_PRIVATE)
+        val uriString = sharedPreferences.getString(imageName, "")
+
+        return if (uriString!!.isEmpty()) null else uriString.toUri()
+    }
+
+    fun saveImageIndex(context: Context, imageName: String, imageIndex: Int) {
+        val sharedPreferences = context.getSharedPreferences("images", Context.MODE_PRIVATE)
+
+        sharedPreferences.edit {
+            putInt(imageName, imageIndex)
+        }
+    }
+
+    fun getImageIndex(context: Context, imageName: String): Int {
+        val sharedPreferences = context.getSharedPreferences("images", Context.MODE_PRIVATE)
+
+        return sharedPreferences.getInt(imageName, 0)
+    }
+
+    fun saveString(context: Context, keyResId: Int, value: String) {
+        val sharedPreferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE)
+        val key = context.getString(keyResId)
+
+        sharedPreferences.edit {
+            putString(key, value)
+        }
+    }
+
+    fun getString(context: Context, keyResId: Int, defaultValueResId: Int = -1): String {
+        val sharedPreferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE)
+        val key = context.getString(keyResId)
+        val defaultValue = if (defaultValueResId == -1) "" else context.getString(defaultValueResId)
+
+        return sharedPreferences.getString(key, defaultValue)!!
+    }
+}
