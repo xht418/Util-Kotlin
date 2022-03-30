3bbeac5d0aae90c76b713c6f5bb3f72b336275d5
Ziliang Chen <c.ym62942@gmail.com>
2022-03-30 16:00:27 -0400

    Update: ConnectivityHelper (1)
    Added 1 helper function called "isAirplaneModeOn()" for getting the state of airplane model

--- utlikotlin/src/main/java/com/example/utlikotlin/ConnectivityHelper.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/ConnectivityHelper.kt
@@ -2,6 +2,7 @@ package com.example.utlikotlin
 
 import android.content.Context
 import android.net.NetworkCapabilities
+import android.provider.Settings
 
 object ConnectivityHelper {
     fun isWifiOn(context: Context) = with(context.getConnectivityManager()) {
@@ -13,6 +14,8 @@ object ConnectivityHelper {
     }
 
     fun isNetworkAvailable(context: Context) = isWifiOn(context) || isDataOn(context)
+
+    fun isAirplaneModeOn(context: Context) = Settings.Global.getInt(context.contentResolver, Settings.Global.AIRPLANE_MODE_ON, 0) != 0
 
     fun getIpAddress(context: Context) = with(context.getConnectivityManager()) {
         getLinkProperties(activeNetwork)!!.linkAddresses[1].address.hostAddress!!
