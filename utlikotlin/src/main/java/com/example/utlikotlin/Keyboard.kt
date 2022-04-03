54aa9410597c6a04e9eb5c0dcdfe307827217e72
Ziliang Chen <c.ym62942@gmail.com>
2022-04-03 10:21:26 -0400

    Add: New Class (1)
    Added another util class called Keyboard (the old one has been renamed to Keyboard2), which contains one helper function call "show()", which is using the latest api to show virtual keyboard.

--- utlikotlin/src/main/java/com/example/utlikotlin/Keyboard.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/Keyboard.kt
@@ -1,27 +1,9 @@
 package com.example.utlikotlin
 
-import android.app.Activity
-import android.content.Context
-import android.view.inputmethod.InputMethodManager
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 object Keyboard {
-    fun hide(activity: Activity) {
-        val inputMethodManager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-
-        activity.currentFocus?.let {
-            inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
-        }
-    }
-
-    fun showInDialog(context: Context) {
-        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-
-        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
-    }
-
-    fun hideInDialog(context: Context) {
-        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-
-        inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
-    }
+    fun show(view: View) = ViewCompat.getWindowInsetsController(view)?.show(WindowInsetsCompat.Type.ime())
 }
---
+++ utlikotlin/src/main/java/com/example/utlikotlin/Keyboard2.kt
@@ -0,0 +1,27 @@
+package com.example.utlikotlin
+
+import android.app.Activity
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+
+object Keyboard2 {
+    fun hide(activity: Activity) {
+        val inputMethodManager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        activity.currentFocus?.let {
+            inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+        }
+    }
+
+    fun showInDialog(context: Context) {
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+    }
+
+    fun hideInDialog(context: Context) {
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+    }
+}
